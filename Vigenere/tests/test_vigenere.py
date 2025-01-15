54cbcea9eb553233ed3da0dab4cd42e4a2f5a7fc
jrdimande <timoteojrdimande@gmail.com>
2025-01-15 23:57:33 +0000

    Cipher Cesar

---
+++ Vigenere/tests/test_vigenere.py
@@ -0,0 +1,20 @@
+import unittest
+from Vigenere.vigenere import CipherVigenere
+
+
+class TestVigener(unittest.TestCase):
+    def test_encrypt(self):
+        cipher = CipherVigenere()
+        result = cipher.encrypt("oi", "key")
+        self.assertEqual(result, "YM")
+
+    def test_decrypy(self):
+        cipher = CipherVigenere()
+        result = cipher.decrypt("YM", "key")
+        self.assertEqual(result, "OI")
+
+
+
+if __name__ == "__main__":
+    unittest.main()
+
