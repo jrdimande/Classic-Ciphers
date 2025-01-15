e1719313bf6bc0b6d32de13adb08eea746d05dab
jrdimande <timoteojrdimande@gmail.com>
2025-01-15 23:57:08 +0000

    Cipher Cesar

---
+++ Vigenere/vigenere.py
@@ -0,0 +1,46 @@
+class CipherVigenere:
+    def __init__(self):
+        self.alphabet = list("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+    """Method to encrypt message"""
+    def encrypt(self, message, key="abc"):
+        encrypted_message = ""
+        message = message.upper()
+        key = key.upper()
+        i = 0
+
+        for char in message:
+            if char in self.alphabet:
+                position = self.alphabet.index(char)
+                key_position = self.alphabet.index(key[i % len(key)])
+                new_position = (position + key_position) % 26
+                new_char = self.alphabet[new_position]
+                encrypted_message += new_char
+                i += 1
+            else:
+                new_message += char
+
+        return encrypted_message
+
+    """Method to decrypt message"""
+    def decrypt(self, message, key):
+        decrypted_message = ""
+        message = message.upper()
+        key = key.upper()
+        i = 0
+
+        for char in message:
+            if char in self.alphabet:
+                position = self.alphabet.index(char)
+                key_position = self.alphabet.index(key[i % len(key)])
+                new_position = (position - key_position) % 26
+                new_char = self.alphabet[new_position]
+                decrypted_message += new_char
+                i += 1
+            else:
+                new_message += char
+
+        return decrypted_message
+
+
+cipher = CipherVigenere()
