e31cd5d2ba8d89cd1cda08d7a4ae8041deb7a8ef
jrdimande <timoteojrdimande@gmail.com>
2025-01-15 23:55:16 +0000

    Cipher Cesar

--- Cesar/cesar.py
+++ Cesar/cesar.py
@@ -4,7 +4,7 @@ class CipherCesar:
         self.key = key
 
     """ Method to encrypt message """
-    def encrypt(self, message, key = 1):
+    def encrypt(self, message, key = 3):
         encrypted_message = ""
 
 
@@ -15,13 +15,23 @@ class CipherCesar:
                 new_char = self.alphabet[new_position]
                 encrypted_message += new_char
 
+            # Save the message in a txt file
+            try:
+                with open('file.txt', 'a') as f:
+                    f.write(encrypted_message)
+            except FileNotFoundError:
+                with open('file.txt', 'a') as f:
+                    f.write(encrypted_message)
+
+
         except ValueError:
             print(f"An error ocurred while trying to encrypt message [{message}], please check if the message contains only letters")
 
         return encrypted_message
 
+
     """ Method to decrypt messages """
-    def decrypt(self, message, key = 1):
+    def decrypt(self, message, key = 3):
         message.upper()
         decrypted_message = ""
 
@@ -31,6 +41,15 @@ class CipherCesar:
                 new_position = (position - key) % 26
                 new_char = self.alphabet[new_position]
                 decrypted_message += new_char
+
+            # Save the message in txt file
+            try:
+                with open('file.txt', 'a') as f:
+                    f.write(decrypted_message)
+            except FileNotFoundError:
+                with open('file.txt', 'a') as f:
+                    f.write(decrypted_message)
+
 
         except ValueError:
             print(f"An error ocurred while trying to decrypt message [{message}], please check if the message contains only letters")
