613b54e4546cf619bf279ff2ef7a82cbf6c5bee4
jrdimande <timoteojrdimande@gmail.com>
2025-01-16 18:07:57 +0000

    Cipher Cesar

---
+++ Cesar/brute_force.py
@@ -0,0 +1,34 @@
+class BruteForce:
+    def __init__(self, message):
+        self.alphabet = list("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+        self.message = message
+
+
+    def decrypt(self,message, key):
+        message = self.message
+        message = message.upper()
+        decrypted_message = ""
+
+        for char in message:
+            if char == ' ':
+                decrypted_message += char
+            else:
+                position = self.alphabet.index(char.upper())
+                new_position = (position - key) % 26
+                new_char = self.alphabet[new_position]
+                if key == 13 and new_char == ' ':
+                    decrypted_message += ' '
+                else:
+                    decrypted_message += new_char
+
+        print(f"[{key}] {decrypted_message}")
+
+
+
+    def run(self):
+        for key in range(26):
+            self.decrypt(self.message, key)
+
+        print("\n\nFinished")
+
+
--- Cesar/main.py
+++ Cesar/main.py
@@ -1,11 +1,10 @@
-from click import option
-
 from cesar import  cipher
+from brute_force import BruteForce
 import pyfiglet
 
 
 text = "Cesar Cipher"
-ascii_art = pyfiglet.figlet_format(text, font="slant")
+ascii_art = pyfiglet.figlet_format(text, font="big")
 print(ascii_art)
 print("Press '0' any time to quit!!")
 
@@ -14,12 +13,12 @@ def encrypt(message, key):
         result = cipher.encrypt(message, int(key))
         print(f"Your Message is now encrypted\n"
  
-              f"Encrypted [Message]: {result}.\n")
+              f"[Message]: {result}.\n")
 # Decrypt messages
 def decrypt(message, key):
     result = cipher.decrypt(message, int(key))
     print(f"Your Message is now decrypted\n\n"
-          f"Decrypted Message: {result}.\n")
+          f"Message: {result}.\n")
 
 # Check if key is valid
 def check_key(key):
@@ -36,32 +35,74 @@ def main():
 
     while flag:
         print("=========================================================================================================")
-        print("[1] Encrypt Message\n"
-              "[2] Decrypt Message")
+        print("=   [1] Encrypt Message                                                                                 =\n"
+              "=   [2] Decrypt Message                                                                                 =\n"
+              "=   [3] Brute Force                                                                                     =\n"
+              "=========================================================================================================")
 
         option = input()
 
+        #Encrpyt
         if option == '1':
+            print("=========================================================================================================")
+            print("++++++++++++++++++++++++++++++++++ Encrypt Messages +++++++++++++++++++++++++++++++++++++++++++++++++++++")
+
             message = input("Enter Message: ")
+            if message == '0':
+                flag = False
             key =input("Enter key: ")
+            if key == '0':
+                flag = False
             valid_key = check_key(key)
 
             while valid_key:
                 key = input("Enter valid key: ")
+                if key == '0':
+                    flag = False
                 valid_key = check_key(key)
 
             encrypt(message, key)
+            print("=========================================================================================================")
 
+
+        # Decrypt
         elif option == '2':
+
+            print("=========================================================================================================")
+            print("++++++++++++++++++++++++++++++++++ Decrypt Messages +++++++++++++++++++++++++++++++++++++++++++++++++++++")
             message = input("Enter Message: ")
+            if message == '0':
+                flag = False
             key = input("Enter key: ")
+            if key == '0':
+                flag = False
             valid_key = check_key(key)
 
             while valid_key:
                 key = input("Enter valid key: ")
+                if key == '0':
+                    flag = False
                 valid_key = check_key(key)
 
             decrypt(message, key)
+            print("=========================================================================================================")
+
+        # Brake Cipher
+        elif option == '3':
+            text = "Brute Force"
+            ascii_art = pyfiglet.figlet_format(text, font="big")
+            print(ascii_art)
+            message = input("Enter Message: ")
+            if message == '0':
+                flag = False
+
+            brute = BruteForce(message)
+            brute.run()
+
+        else:
+            if option == '0':
+                flag = False
+
 
 
 
